7993f921852aa2faee729a6072f4bc59abc3f1fa
Christoph Keller <gri@not-censored.com>
2015-07-10 13:18:00 +0200

    Added initial migrations.

---
+++ testproject/simple_app/migrations/0001_initial.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('text', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Owner',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Pizza',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Topping',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='VirtualMachine',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('cpus', models.IntegerField()),
+                ('so', models.CharField(max_length=100)),
+                ('started', models.BooleanField()),
+                ('owner', models.ForeignKey(to='simple_app.Owner')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='pizza',
+            name='toppings',
+            field=models.ManyToManyField(to='simple_app.Topping'),
+        ),
+    ]
